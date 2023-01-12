f88fe260573e318cc64cd6a3f1a7f1c11444b474
binhudakhalid <khalid.bin.huda@hotmail.com>
2023-01-12 01:16:00 +0100

    intial commit

---
+++ onesided/e1_j_accumulate_latency_fence.jl
@@ -0,0 +1,10 @@
+using MPIBenchmarks
+benchmark(OSUAccumulateLatency(Int8; synchronization_option = "fence", max_size=2097152,filename="e1_j_accumulate_latency_lock.csv", iterations=262144))
+
+
+
+
+
+
+
+
