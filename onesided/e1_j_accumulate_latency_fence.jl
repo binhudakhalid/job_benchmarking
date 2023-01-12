03ff827fcfffe91b887099f3b6f40d29ea56b1e1
binhudakhalid <khalid.bin.huda@hotmail.com>
2023-01-12 01:31:56 +0100

    update few lines

--- onesided/e1_j_accumulate_latency_fence.jl
+++ onesided/e1_j_accumulate_latency_fence.jl
@@ -1,10 +1,2 @@
 using MPIBenchmarks
 benchmark(OSUAccumulateLatency(Int8; synchronization_option = "fence", max_size=2097152,filename="e1_j_accumulate_latency_lock.csv", iterations=262144))
-
-
-
-
-
-
-
-
--- onesided/e1_j_put_latency_fence.jl
+++ onesided/e1_j_put_latency_fence.jl
@@ -1,10 +1,2 @@
 using MPIBenchmarks
-benchmark(OSUPutLatency(Int8; synchronization_option = "fence", max_size=2097152,filename="e1_j_put_latency_lock.csv", iterations=262144))
-
-
-
-
-
-
-
-
+benchmark(OSUPutLatency(Int8; synchronization_option = "fence", max_size=2097152,filename="e1_j_put_latency_fence.csv", iterations=262144))
